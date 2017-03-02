8806fcad562795717487b868d15269fe7c61bd78
Brian Fjeldstad <bfjelds@microsoft.com>
2017-03-02 11:21:29 -0800

    remove appservice from background app

--- samples/IoTDMBackground/DMClientBackgroundApplication.cs
+++ samples/IoTDMBackground/DMClientBackgroundApplication.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
+﻿using Windows.ApplicationModel.Background;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Devices.Management;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Microsoft.Azure.Devices.Shared;
-using Windows.ApplicationModel.AppService;
-using Windows.Foundation.Collections;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -35,33 +28,9 @@ namespace IoTDMBackground
         }
 
         // Answer the question "is it OK to reboot the toaster"
-        async Task<SystemRebootRequestResponse> IDeviceManagementRequestHandler.IsSystemRebootAllowed()
+        Task<SystemRebootRequestResponse> IDeviceManagementRequestHandler.IsSystemRebootAllowed()
         {
-            bool answer = true;
-
-            // get answer from any AppService connections
-            foreach (var connection in DMClientBackgroundApplication.Instance.Connections)
-            {
-                var status = await connection.OpenAsync();
-                if (status != AppServiceConnectionStatus.Success)
-                {
-                    var message = new ValueSet();
-                    message.Add("get", "readyForReboot");
-                    var result = await connection.SendMessageAsync(message);
-                    if (result.Status == AppServiceResponseStatus.Success)
-                    {
-                        var response = result.Message;
-                        bool readyForReboot = (bool)response["readyForReboot"];
-                        if (!readyForReboot)
-                        {
-                            answer = false;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return answer ? SystemRebootRequestResponse.Accept : SystemRebootRequestResponse.Reject;
+            return Task.FromResult(SystemRebootRequestResponse.Accept);
         }
     }
 
@@ -97,54 +66,10 @@ namespace IoTDMBackground
             await deviceClient.SetDesiredPropertyUpdateCallback(OnDesiredPropertyUpdate, null);
         }
 
-        public DMClientBackgroundApplication()
-        {
-            Connections = new List<AppServiceConnection>();
-            DMClientBackgroundApplication.Instance = this;
-        }
-        public static DMClientBackgroundApplication Instance { get; private set; }
-        public IList<AppServiceConnection> Connections { get; private set; }
-
-        internal async Task<string> ExecuteDMRequest(string request, string data)
-        {
-            try
-            {
-                if (request.Equals("checkForUpdates"))
-                {
-                    return (await _dmClient.CheckForUpdatesAsync()).ToString();
-                }
-                else if (request.Equals("immediateReboot"))
-                {
-                    await _dmClient.ImmediateRebootAsync();
-                    return true.ToString();
-                }
-                else if (request.Equals("factoryReset"))
-                {
-                    await _dmClient.DoFactoryResetAsync();
-                    return true.ToString();
-                }
-            } catch (Exception)
-            {
-            }
-            return false.ToString();
-        }
-
         private async Task OnDesiredPropertyUpdate(TwinCollection desiredProperties, object userContext)
         {
             // Let the device management client process properties specific to device management
             _dmClient.ProcessDeviceManagementProperties(desiredProperties);
-
-            // Application developer can process all the top-level nodes here
-            foreach (var connection in Connections)
-            {
-                var status = await connection.OpenAsync();
-                if (status != AppServiceConnectionStatus.Success)
-                {
-                    var message = new ValueSet();
-                    message.Add("desiredPropertyUpdate", desiredProperties.ToJson());
-                    var result = await connection.SendMessageAsync(message);
-                }
-            }
         }
     }
 }
