1f15467739d9b47728d6d0dddf171cb2d92258fd
George Mileka <gmileka@outlook.com>
2017-09-14 23:03:07 -0700

    Update the nuget to include the new proxy dll/winmd.

--- nuget/PackIoTDMClientLib.cmd
+++ nuget/PackIoTDMClientLib.cmd
@@ -1,5 +1,25 @@
+@echo off
 @REM Examples:
 @REM   PackIoTDMClientLib.cmd 1.4.4 Release
 @REM   PackIoTDMClientLib.cmd 1.4.4 Debug
 
+@if "%1"=="" goto MissingParameters
+@if "%2"=="" goto MissingParameters
+
 NuGet.exe pack "IoTDMClientLib.nuspec" -Prop Version=%1 -Prop Flavor=%2
+goto End
+
+:MissingParameters
+@echo.
+@echo Usage:
+@echo     PackIoTDMClientLib.cmd version flavor
+@echo.
+@echo where:
+@echo     version: the version of the nuget package to be genered in the form: major.minor.revision.
+@echo     flavor : Debug or Release
+@echo.
+@echo Example:
+@echo     PackIoTDMClientLib.cmd 1.4.4 Release
+@echo.
+
+:End
