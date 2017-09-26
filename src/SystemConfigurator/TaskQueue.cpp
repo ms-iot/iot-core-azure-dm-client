1b5d4aa879d11899e81f079860df55075f1ea4b9
George Mileka <gmileka@outlook.com>
2017-09-26 14:20:27 -0700

    All MDM calls need to happen on the same thread. Fix for 'catastrophic failure' when calling RegisterDeviceWithLocalManagement/ApplyLocalManagementSyncML.

---
+++ src/SystemConfigurator/TaskQueue.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "TaskQueue.h"
+#include "../SharedUtilities/Logger.h"
+#include "../SharedUtilities/DMException.h"
+
+using namespace std;
+
+TaskQueue::TaskQueue()
+{
+}
+
+future<wstring> TaskQueue::Enqueue(Task task)
+{
+    TRACE(__FUNCTION__);
+
+    unique_lock<mutex> l(_mutex);
+
+    future<wstring> response = task.get_future();
+    _queue.push(move(task));
+
+    l.unlock();
+    _cv.notify_one();
+
+    return response;
+}
+
+TaskQueue::Task TaskQueue::Dequeue()
+{
+    TRACE(__FUNCTION__);
+
+    unique_lock<mutex> l(_mutex);
+    _cv.wait(l, [&] { return !_queue.empty(); });
+
+    Task taskItem = move(_queue.front());
+    _queue.pop();
+
+    return taskItem;
+}
